996900c25816a333a032204e576c1667ab551b62
King <yubing429898077@gmail.com>
2017-11-14 13:12:51 +0800

    2017/11/14

---
+++ app/src/main/java/com/itcast/iraqishome/fragment/BaseFragment.java
@@ -0,0 +1,28 @@
+package com.itcast.iraqishome.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yb on 2017/11/14.
+ */
+
+public abstract class BaseFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    public abstract View initView();
+}
